f242494d311edd72c13d9df5c9f5b85ec146849e
Francesca Polzella <franpolzella@gmail.com>
2024-10-09 01:42:23 -0400

    finalizado

--- APIJS/js/speechchAPI.js
+++ APIJS/js/speechchAPI.js
@@ -9,28 +9,40 @@ function ejecutarSpeechAPI(){
     const recognition= new speechrecognition()
 
     /*
-frases 
--arrancar ,ejecutar reconocimientos
--comenzar y escuchar
--usuario termine de hablar 
--mostrar el usuario (texto)
-*/
+ frases 
+ -arrancar ,ejecutar reconocimientos
+ -comenzar y escuchar
+ -usuario termine de hablar 
+ -mostrar el usuario (texto)
+ */
 
-recognition.start()
-recognition.onstart=function(){
-    //comience a escuchar
-    salida.classList.add('mostrar')
-}
-salida.textContent='Escuchando...'
+ recognition.start()
+ recognition.onstart=function(){
+     //comience a escuchar
+     salida.classList.add('mostrar')
+    }
+ salida.textContent='Escuchando...'
 
-recognition.onspeechend= function(){
-    salida.textContent='Se dejo de escuchar'
-    recognition.stop()
-}
+ recognition.onspeechend= function(){
+     salida.textContent='Se dejo de escuchar'
+     recognition.stop()
+    }
 
-recognition.onresult=function(e){
-    console.log(e.results)
-}
+ recognition.onresult=function(e){
+      //console.log(e.results)
+      var transcript = e. results [0][0].transcript
+      var confidence = e.results[0][0].confidence
+
+      const sppech = document.createElement('p')
+      sppech.innerHTML= `Lo que se grabo: ${transcript}`
+
+      const confianza =document.createElement('p')
+      confianza.innerHTML= `Confianza ${parseInt(confidence*100)}%`
+
+      salida.appendChild(sppech)
+      salida.appendChild(confianza)
+
+    }
 
 }
 
